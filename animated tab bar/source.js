6aeb9c627eb959ab83dea8e6e246278962a5b37b
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-10-10 14:27:33 +0200

    Update source.js

--- animated tab bar/source.js
+++ animated tab bar/source.js
@@ -7,3 +7,10 @@ const menuItems = menu.querySelectorAll(".menu__item");
 const menuBorder = menu.querySelector(".menu__border");
 
 let activeItem = menu.querySelector(".active");
+
+function clickedItem(item, index) {
+    menu.style.removeProper("--timeOut");
+
+    if (activeItem == item) { return; }
+    if (activeItem) { activeItem.classList.remove("active"); }
+}
