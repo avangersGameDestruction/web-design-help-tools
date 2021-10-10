971c9f7103367057b1b0e92c1b213885d4260c60
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-10-10 18:20:14 +0200

    Update source.js

---
+++ radial progressbar js only/source.js
@@ -0,0 +1,13 @@
+var RadialProgress = function(size, barSize, barColor, backgroundColor, textColor, zIndex) {
+    this.radialProgress = document.createElement('div');
+    this.style = document.createElement('style');
+    this.progress = 0;
+
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozAnimationFrame ||
+        window.webkitAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
+
+}
